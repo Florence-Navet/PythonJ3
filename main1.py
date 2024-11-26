26628ab00f7671bec700e0ff129fa208deba47fe
AeRiaSaito <florence.navet@laplateforme.io>
2024-11-26 10:24:10 +0100

    job 1 et 2

---
+++ main1.py
@@ -0,0 +1,12 @@
+#Job1
+#Demandez à l'utilisateur d'entrer deux valeurs
+valeur1 = input("Entrez la première valeur : ")
+valeur2 = input("Entrez la deuxième valeur : ")
+
+#Vérifions l'égalité des valeurs
+if valeur1 == valeur2:
+    print("Valeur1 est égal à valeur 2.")
+else:
+    print("Les deux valeurs ne sont pas égales.")
+
+    
---
+++ main2.py
@@ -0,0 +1,8 @@
+#Initialisation de la variable
+age = int(input("Entrez votre age : "))
+
+#Verificat si l'age est au moins 18 ans
+if age >= 18:
+    print("Tu peux voter")
+else:
+    print("Tu ne peux pas voter")    
