2025d076f36888a5d2400e692a7d3219b2f111e6
AeRiaSaito <florence.navet@laplateforme.io>
2024-11-26 12:00:16 +0100

    job 6

---
+++ main6.py
@@ -0,0 +1,6 @@
+#DEmande à l'utilisateur de rentrer un nb
+nombre = int(input("Veuillez entrer un nombre ?"))
+if nombre % 2 == 0:
+    print(f"Je suis un nombre pair.")
+else:
+    print(f"Je suis un nombre impair.")    
